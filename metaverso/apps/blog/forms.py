488ccb1d50e45c58a4b7b29a9974620667f9d419
jacomba9 <carlosacomba@gmail.com>
2023-07-18 18:48:39 -0300

    creacion de las clases categoria y comentario, agregado de formularios para noticia,categoria y comentario

--- metaverso/apps/blog/forms.py
+++ metaverso/apps/blog/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from .models import Usuario
+from .models import Usuario,Noticia,Comentario,Categoria
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
-
 
 class Usuarioform(UserCreationForm):
     class Meta:
@@ -28,3 +27,28 @@ class Usuarioform(UserCreationForm):
         user.save()
         return user
     
+
+class NoticiaForm(forms.ModelForm):
+    class Meta:
+        model = Noticia
+        fields = ("titulo", "detalle", "imagen","categoria")
+    def __init__(self, *args, **kwargs):
+        super(NoticiaForm, self).__init__(*args, **kwargs)
+        self.fields["titulo"].widget.attrs.update({'placeholder' : 'titulo del post', 'type' : 'text'})
+        self.fields["detalle"].widget.attrs.update({'placeholder' : 'Escriba su Noticia:', 'type' : 'text'})
+        self.fields["imagen"].widget.attrs.update({'placeholder' : '', 'type' : 'file'})
+        self.fields["categoria"].widget.attrs.update({'placeholder' : 'Ingrese Categoria', 'type' : ''})
+
+class ComentarioForm(forms.ModelForm):
+    class Meta:
+        model = Comentario
+        fields =("texto",)
+    def __init__(self, *args, **kwargs):
+        super(ComentarioForm, self).__init__(*args, **kwargs)
+        self.fields["texto"].widget.attrs.update({'class' : 'materialize-textarea','placeholder' : 'Escriba su comentario', 'type' : 'text'})
+
+#se puede cargar por bd y no se hace la vista para admin
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ("titulo","descripcion")
--- metaverso/apps/blog/models.py
+++ metaverso/apps/blog/models.py
@@ -6,12 +6,10 @@ class Noticia(models.Model):
     detalle = models.TextField()
     imagen = models.ImageField(upload_to='blog', default='blog/img.jpg')
     fecha_publicacion= models.DateTimeField(auto_now_add=True)
-    
+    fecha_modificado = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey("Categoria", on_delete = models.SET_NULL,null=True)
     def __srt__(self):
         return self.titulo
-    
-
-
 
 class Usuario(AbstractUser):  
     nombre = models.CharField(max_length=20, null=True) 
@@ -20,10 +18,19 @@ class Usuario(AbstractUser):
     es_colaborador = models.BooleanField('es_colaborador', default=False)
     imagen = models.ImageField(upload_to='blog')
     email = models.EmailField(null=True, blank=True)
-
-    
-    
     #def __srt__(self):
         #return self.nombre
+
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    coticia=models.ForeignKey(Noticia, on_delete = models.CASCADE)
+    usuario=models.ForeignKey(Usuario, on_delete = models.SET_NULL, null=True)
+    texto = models.TextField(max_length=200)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
     
 
